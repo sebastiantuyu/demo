e0d41abccd84df2b11b723c5bea197c3d2a8e8e1
sebastiantuyu <alextpineiro@gmail.com>
2021-05-12 20:18:08 -0500

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router,
-         Switch,
-         Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Landing from './components/Landing.js';
-import Contact from './components/Contact.js';
-import MakeProp from './pages/MakeProp.js';
-import Profile from  './components/Profile.js';
-import Footer from './components/Footer.js';
-import PropViewer from './components/PropViewer.js';
-import AllProposals from './pages/AllProposals';
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Landing from "./pages/Landing.js";
+import Contact from "./pages/Contact.js";
+import MakeProp from "./pages/MakeProp.js";
+import Profile from "./pages/Profile.js";
+import AllProposals from "./pages/AllProposals";
+import PropViewer from "./components/PropViewer.js";
+import Footer from "./components/Footer.js";
 
 export default function App() {
   return (
-      <Router>
-        <Nav />        
-          <Switch>
-    {/* OK*/} <Route exact path="/" component={Landing} />
-    {/* OK*/} <Route exact path="/contacto" component={Contact}/>
-              <Route exact path="/crear-propuesta" component={MakeProp} />
-    {/* OK*/} <Route exact path="/perfil/propuestas" component={Profile} />
-    {/* OK*/} <Route exact path="/propuestas/:id" component={PropViewer} />
-    {/* OK*/} <Route exact path="/all" component={AllProposals} />
-          </Switch>
-        <Footer avoid={["contacto","crear-propuesta"]}/>
-      </Router>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/contacto" component={Contact} />
+        <Route exact path="/crear-propuesta" component={MakeProp} />
+        <Route exact path="/perfil/propuestas" component={Profile} />
+        <Route exact path="/propuestas/:id" component={PropViewer} />
+        <Route exact path="/all" component={AllProposals} />
+      </Switch>
+      <Footer avoid={["contacto", "crear-propuesta"]} />
+    </Router>
   );
 }
-
